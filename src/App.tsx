5be1a94e087253e69ad1c99e61e38720ff750230
occlairmont <occlairmont@gmail.com>
2020-09-12 15:49:43 -0400

    swtiching branches

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello
-  test2 (octavia)
+  return (
+  <div className="App">Hello
+  test2 
   </div>;
+  )
 }
 
 export default App;
