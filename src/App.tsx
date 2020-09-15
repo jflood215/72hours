eae15a14c7bb79cb56710aabb64afc5ba981f575
John Flood <jflood215@gmail.com>
2020-09-15 10:29:37 -0400

    nasa image display

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
+import NasaAPI from "../src/Nasa/NasaAPI";
 
 function App() {
-  return <div className="App">Hello
-    test: (Kayla) </div>
-  return <div className="App">Johns Branch!</div>;
-  return <div className="App">Hello
-  test2 (octavia)
-  </div>;
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+  const nasaUrl = `https://api.nasa.gov/planetary/earth/imagery?lon=${longitude}&lat=${latitude}&date=2020-02-01&api_key=e4nE0ZmWOYdjGYysdCAvpCTn4ZBQWj34uqUca3Nc`;
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(location);
+  }, []);
+
+  function location(position: any) {
+    setLatitude(position.coords.latitude);
+    setLongitude(position.coords.longitude);
+  }
+
+  return (
+    <div>
+      <NasaAPI url={nasaUrl} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Nasa/NasaAPI.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+export interface NasaProps {
+  url: string;
+}
+
+export interface NasaState {
+  latitude: number;
+  longitude: number;
+  date: Date;
+  nasaBlob: string;
+}
+
+class Nasa extends React.Component<NasaProps, NasaState> {
+  constructor(props: NasaProps) {
+    super(props);
+    this.state = {
+      latitude: 0,
+      longitude: 0,
+      date: new Date(),
+      nasaBlob: "",
+    };
+  }
+
+  componentDidUpdate(prevProps: NasaProps) {
+    // Typical usage (don't forget to compare props):
+    if (this.props.url !== prevProps.url) {
+      fetch(this.props.url)
+        .then((res) => res.blob())
+        .then((data: Blob) => {
+          this.setState({ nasaBlob: URL.createObjectURL(data) });
+          //   myImage.src = objectURL;
+          console.log(data);
+        });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <img
+          style={{ maxWidth: "300px", maxHeight: "400px" }}
+          src={this.state.nasaBlob}
+        />
+      </div>
+    );
+  }
+}
+
+export default Nasa;
