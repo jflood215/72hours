63d1a3bb0dffaa33866756b021d40f6bdc1a5470
Kayla Bullard <kaylabullard79@gmail.com>
2020-09-15 00:23:37 -0400

    Help

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import Zomato from './Zomato';
+
 
 function App() {
-  return <div className="App">Hello
-    test: (Kayla) </div>
-  return <div className="App">Johns Branch!</div>;
-  return <div className="App">Hello
-  test2 (octavia)
-  </div>;
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+  const RestaurantURL = `https://developers.zomato.com/api/v2.1/geocode?lat=${latitude}&lon=${longitude}`
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(location);
+  }, []);
+  function location(position: any) {
+    setLatitude(position.coords.latitude);
+    setLongitude(position.coords.longitude);
+    console.log("Lat: " + latitude);
+    console.log("Lon: " + longitude);
+  }
+  
+  return (
+    <div>
+      <Zomato URL={RestaurantURL}  />
+    </div>
+  )
 }
+
 
 export default App;
